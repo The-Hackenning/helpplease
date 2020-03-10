aaf4bdb33d6fca567f01c7eebdd17967af0326dc
ZagZero <s254810@studenti.polito.it>
2020-03-09 22:51:33 -0500

    Server Connection screen started

--- lib/EnterCallsign.dart
+++ lib/EnterCallsign.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'main.dart';
-import 'loadoutInfo.dart';
 
 class EnterCallsign extends StatefulWidget {
   @override
